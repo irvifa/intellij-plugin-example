2892ce58437bf093e4531e40a9acf443874ba495
GitHub Action <action@github.com>
2022-01-29 12:02:38 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/irvifa/intellijpluginexample/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.irvifa.intellijpluginexample.services
+
+import com.github.irvifa.intellijpluginexample.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
